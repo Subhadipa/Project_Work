bea8ba7660613f167a5ae59ae3fb5d8b6e0e2b44
Subhadipa <bsubhadipa@gmail.com>
2022-01-11 12:47:21 +0530

    Project Done

--- controllers/jsonToexcel.js
+++ controllers/jsonToexcel.js
@@ -1,16 +1,11 @@
-const XLSX = require('xlsx')
+const {google}=require("googleapis")
 
-//const data=require("../sheetData.json")
-const _ = require('lodash');
-
-
-exports.createExcel = (req,res)=>  
+const getSheet= async function(req,res)
 {
- 
- var data =req.files.jsonFile.data;
- //console.log(data)
-var arr = new Array();
-//console.log(arr)
+    try{
+    let data =req.files.jsonFile.data;
+    var arr = new Array();
+
 
 for(let i = 0; i!= data.length;i++)
 {
@@ -20,19 +15,115 @@ for(let i = 0; i!= data.length;i++)
 }
 
 var bstr = arr.join("");
+
 var bstr1=JSON.parse(bstr)
+/*const result = bstr1.map(value => ((Object.entries(value))))                  //.toString().split(","));
+for(let j=0;j!=result.length;j++)
+{
+  for(let j1=0;j1!=result[j].length;j1++)
+    {
+      result[j][0]=((result[j][0])).join(",").split(",");                            //.toString()
+     // var finalData = result[j][0]
+    }
+}*/
+//console.log(result)
+//console.log((JSON.parse(result)))
+//console.log(JSON.parse(result))
 //console.log(bstr1)
-
-const workSheet = XLSX.utils.json_to_sheet(bstr1);
-//console.log(workSheet)
-
-const workBook = XLSX.utils.book_new();
-//console.log(workBook)
-
- XLSX.utils.book_append_sheet(workBook, workSheet, "Data By Teesta")
-// Binary string
- XLSX.write(workBook, { bookType: "xlsx", type: "binary" })
-
- XLSX.writeFile(workBook, "Subhadipa_sheetData.xlsx")
- return res.status(201).send({status:"true",msg:"Excel file created Successfully"})
+for(let i=0;i!=bstr1.length;i++)
+{
+    
+    
+     columns=Object.keys(bstr1[i])
+    
+    
 }
+//console.log(columns)
+
+
+
+let rows=[]
+let row
+//for (let key in bstr1) 
+ //{
+  //   let keyValues = bstr1[key];
+
+   //  rows[key]=(Object.values(keyValues))
+    
+   //  let rowsValue1=JSON.parse(rows[key])
+   // rows.push(rowsValue1);
+  /*  if(typeof rows[key]==Number)
+    {
+        rows[key]=rows[key].toString()
+    }
+ for (var prop in  keyValues) 
+    {
+        
+       if(keyValues.hasOwnProperty(prop))
+       {
+      
+        
+         row=(keyValues[prop])
+        rows.push(row);
+        }
+      
+        //rows1=[...rows]
+         
+       }*/
+//}
+
+
+//console.log(rows)
+
+//console.log(result);
+//var bstr1000=JSON.parse(rows)
+//console.log(rows)
+//console.log(bstr1)
+    const auth=new google.auth.GoogleAuth({
+
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+});
+ // Create client instance for auth
+ const client = await auth.getClient();
+
+ // Instance of Google Sheets API
+ const googleSheets = google.sheets({ version: "v4", auth: client });
+
+ const spreadsheetId = "13wDKeTDCHgt0kAXdK7pAGZWjSSu0nyoX6EbZr7kSVhg";
+// Get metadata about spreadsheet
+const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  //console.log(metaData.data)
+  //res.send({msg:metaData})
+
+ // Read rows from spreadsheet
+ const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1",
+  });
+ // return res.send(getRows.data);
+// Write row(s) to spreadsheet
+const getRows1=await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Sheet1",
+    valueInputOption: "USER_ENTERED",
+  
+    
+   resource: {  values: [columns]},//[result]
+  });
+  //return res.send(getRows1.auth);
+  return res.status(201).send({status:"true",msg:"Excel file created Successfully"})
+    }
+    catch(err)
+    {
+        return res.status(500).send({status:"false",msg:err.message})
+    }
+}
+
+module.exports={getSheet}
--- routes/route.js
+++ routes/route.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const JtoEController= require("../controllers/jsonToexcel")
 
 
-router.post('/createExcel',JtoEController.createExcel);
+router.post('/getExcel',JtoEController.getSheet);
 
 module.exports = router;
